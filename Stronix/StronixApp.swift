bc80490d710ca4b27d37d7e6f8e4651fcd7b5988
lutzzdias <thiagolutzdias@hotmail.com>
2024-08-10 00:55:41 -0400

    Initial Commit

---
+++ Stronix/StronixApp.swift
@@ -0,0 +1,17 @@
+//
+//  StronixApp.swift
+//  Stronix
+//
+//  Created by Thiago Dias on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StronixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
